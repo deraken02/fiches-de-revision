1a6704fddb3a64d2e77ec845ac5fd1808710f488
Louis Delacroix <louis.delacroix.etu@univ-lille.fr>
2021-06-27 18:28:04 +0200

    Fin du main

---
+++ src/main.py
@@ -0,0 +1,68 @@
+import create
+import challenge
+import theme
+
+def main():
+    print("[r] Pour commencer à révision")
+    print("[t] Pour créer un thème")
+    print("[f] Pour créer une fiche")
+    print("[h] Pour accéder à l'aide")
+    choisi=True
+    while(choisi):
+        choix=input("Votre choix: ")
+        if choix=='r':
+            choisi=False
+            revise()
+        elif choix=='t':
+            choisi=False
+            thème()
+        elif choix=='f':
+            choisi=False
+            fiche()
+        elif choix=='h':
+            choisi=False
+            aide()
+        else:
+            print("Ce choix n'existe pas")
+                
+def revise():
+    """
+    Lance le module de challenge
+    """
+    ntheme=input("Thème: ")
+    c=challenge.challenge(ntheme)
+    print(c.start())
+    main()
+
+def thème():
+    """
+    Lance la création d'un thème
+    """
+    ntheme=input("Thème: ")
+    c=theme.theme(ntheme)
+    c.createtheme()
+    print("Le thème "+ntheme+" a été créé")
+    main()
+
+def fiche():
+    """
+    Lance la création d'une fiche
+    """
+    ntheme=input("Thème: ")
+    face1=input("Première face: ")
+    face2=input("Seconde face ")
+    c=create.create(face1,face2,ntheme)
+    c.createfiche()
+    print("Votre fiche à bien été créé")
+    main()
+
+def aide():
+    """
+    Affiche l'aide
+    """
+    print("Cette section est en cours de construction")
+    main()
+    
+    
+print("Bienvenue sur le logiciel de révision")
+main()
